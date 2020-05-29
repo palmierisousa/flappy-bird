a1f2a7056ca9835f80fb0a07a0cfac85199be5db
Marcelo Palmieri <palmierisousa@gmail.com>
2020-05-28 21:50:05 -0300

    Making static only the necessary files.

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const path = require('path')
 
 const app = express()
 
-app.use(express.static('.'))
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'flappy.html'))
 })
+
+app.use('/css', express.static('css'))
+app.use('/fonts', express.static('fonts'))
+app.use('/imgs', express.static('imgs'))
+app.use('/js', express.static('js'))
 
 app.listen(process.env.PORT || 8080, () => console.log('Executando...'))
